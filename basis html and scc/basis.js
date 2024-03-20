8a4eb5e3a8be71954764b9a1f7effc542267feb4
janak-ojha <janakrajojha66@gmail.com>
2024-03-20 10:39:59 +0530

    hello

---
+++ basis html and scc/basis.js
@@ -0,0 +1,30 @@
+ 
+ 
+ let Car={
+    Brand:"tata",
+    Engine:1289,
+    Color:"black",
+    Year:"2024",
+    Fuel_type:"Electric"
+}
+
+let Person={
+    Name:"janak",
+    Age:23,
+    cource:"btech",
+    contact_no:7906568493
+}
+
+
+ cars=["bmw","audi","skoda"]
+console.log(cars.slice(0,2))
+console.log(cars.slice(1))
+//splice
+let months=["jan","feb","mar","apri"];
+Document.write(months.splice(0,1,"august"))
+console.log(months.splice(1,0,"august"))
+
+
+
+
+
